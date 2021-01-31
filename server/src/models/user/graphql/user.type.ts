9f5fb3ef95c2c841adec00c958811ab9f5090ad3
ChristopherZinger <krzysztof.zinger@gmail.com>
2021-01-31 19:50:25 +0100

    add userProfile to user.types.ts

--- server/src/models/user/graphql/user.type.ts
+++ server/src/models/user/graphql/user.type.ts
@@ -1,18 +1,17 @@
-import {ObjectType, Field, ID} from "type-graphql";
-import {Student} from "../../student/model/Student.model";
+import { ObjectType, Field, ID } from "type-graphql";
+import { UserProfile } from "../../userProfile/model/Student.model";
 
 @ObjectType()
 export class UserType {
-    @Field(()=>ID)
-    id!: number;
+	@Field(() => ID)
+	id!: number;
 
-    @Field()
-    email!: string;
+	@Field()
+	email!: string;
 
-    @Field()
-    password!: string;
+	@Field()
+	password!: string;
 
-    @Field(()=> Student, {nullable: true})
-    user?: Student;
+	@Field(() => UserProfile, { nullable: true })
+	user?: UserProfile;
 }
-

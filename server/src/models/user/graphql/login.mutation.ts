a3679dd6f6913ee37bcc0e7f2f2fbfd3edef1ad1
ChristopherZinger <krzysztof.zinger@gmail.com>
2021-02-01 14:41:06 +0100

    add input validation to login and register

--- server/src/models/user/graphql/login.mutation.ts
+++ server/src/models/user/graphql/login.mutation.ts
@@ -1,20 +1,33 @@
-import { Resolver, Mutation, Ctx, Arg } from "type-graphql";
+import { Resolver, Mutation, Ctx, Arg, Field, InputType } from "type-graphql";
+import { IsEmail, Length } from "class-validator";
 import { getRepository } from "typeorm";
 import { UserResponse } from "./register.mutation";
 import { ContextType } from "../../../core/create-gql-context";
 import bcrypt from "bcryptjs";
 import { User } from "../model/User.model";
 
+@InputType()
+class LoginInputType {
+	@Field(() => String)
+	@Length(8, 255, {
+		message: "Incorrect password length. Has to be between 8 and 255 charactes."
+	})
+	password!: string;
+
+	@Field(() => String)
+	@IsEmail()
+	email!: string;
+}
+
 @Resolver()
 export class LoginMutation {
 	@Mutation(() => UserResponse, { nullable: true })
 	async login(
-		@Arg("email") email: string,
-		@Arg("password") password: string,
+		@Arg("data") data: LoginInputType,
 		@Ctx() ctx: ContextType
 	): Promise<UserResponse | null> {
 		const userRepository = getRepository(User);
-		const lowerCaseEmail = email.toLowerCase();
+		const lowerCaseEmail = data.email.toLowerCase();
 		const user = await userRepository.findOne({
 			where: { email: lowerCaseEmail }
 		});
@@ -24,7 +37,7 @@ export class LoginMutation {
 			return null;
 		}
 
-		const valid = bcrypt.compareSync(password, user.password);
+		const valid = bcrypt.compareSync(data.password, user.password);
 
 		if (!valid) {
 			console.warn("Wrong credentials: password.");
--- server/src/models/user/graphql/register.mutation.ts
+++ server/src/models/user/graphql/register.mutation.ts
@@ -1,5 +1,14 @@
 import { ContextType } from "@/core/create-gql-context";
-import { Arg, Ctx, Field, Mutation, ObjectType, Resolver } from "type-graphql";
+import { IsEmail, Length } from "class-validator";
+import {
+	Arg,
+	Ctx,
+	Field,
+	InputType,
+	Mutation,
+	ObjectType,
+	Resolver
+} from "type-graphql";
 import { User, UserRole } from "../model/User.model";
 import bcrypt from "bcryptjs";
 import { getRepository } from "typeorm";
@@ -10,24 +19,38 @@ export class UserResponse {
 	email!: string;
 }
 
+@InputType()
+class RegistrationInputType {
+	@Field(() => String)
+	@Length(8, 255, {
+		message: "Incorrect password length. Has to be between 8 and 255 charactes."
+	})
+	password!: string;
+
+	@Field(() => String)
+	@IsEmail()
+	email!: string;
+}
+
 @Resolver()
 export class RegisterMutation {
 	@Mutation(() => UserResponse)
 	async register(
-		@Arg("email") email: string,
-		@Arg("password") password: string,
+		@Arg("data") data: RegistrationInputType,
 		@Ctx() ctx: ContextType
 	): Promise<UserResponse> {
 		const userRepository = getRepository(User);
-		const userExist = await userRepository.findOne({ where: { email } });
+		const userExist = await userRepository.findOne({
+			where: { email: data.email }
+		});
 
 		if (userExist) {
 			throw new Error("user with this email already exists");
 		}
 
 		const user = new User();
-		user.email = email.toLocaleLowerCase();
-		user.password = bcrypt.hashSync(password, bcrypt.genSaltSync(15));
+		user.email = data.email.toLocaleLowerCase();
+		user.password = bcrypt.hashSync(data.password, bcrypt.genSaltSync(15));
 		user.role = UserRole.BASE_USER;
 		const newUser = await userRepository.save(user);
 

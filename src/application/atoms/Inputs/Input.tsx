ec69fc36aadc0c627537d5975623e49b4adc4f6e
ChristopherZinger <krzysztof.zinger@gmail.com>
2021-02-03 22:21:16 +0100

    create styled input

---
+++ src/application/atoms/Inputs/Input.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Input = styled.input`
+    border: none !important;
+    border-radius: 0px !important;
+    border-bottom: 1px solid black !important;
+    background-color: transparent !important;
+    font-size: 20px !important;
+`

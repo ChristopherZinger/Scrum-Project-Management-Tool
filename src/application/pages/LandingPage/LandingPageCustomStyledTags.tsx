1b632506254499aba87b5031da902bc5d824ab53
ChristopherZinger <krzysztof.zinger@gmail.com>
2021-02-03 22:22:08 +0100

    remove redundant imports

--- src/application/pages/LandingPage/LandingPageCustomStyledTags.tsx
+++ src/application/pages/LandingPage/LandingPageCustomStyledTags.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import landingPageImage from "../../images/landing-page-scrum.svg"
 import { mobileBreakPoint, tabletBreakPoint, computerBreakPoint } from "../../atoms/style";
 

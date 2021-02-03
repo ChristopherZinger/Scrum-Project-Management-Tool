7908a1659036cca1da5cc0e495b7a680e7d315d5
ChristopherZinger <krzysztof.zinger@gmail.com>
2021-02-03 13:05:58 +0100

    margin correction to nav; landing page structure correction; move nav to components

--- src/application/components/Navbar/Navbar.tsx
+++ src/application/components/Navbar/Navbar.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Grid } from "semantic-ui-react";
-import { UnderlineLink } from "../../../atoms/Links/UnderlineLink";
+import { UnderlineLink } from "../../atoms/Links/UnderlineLink";
 
 
 export const Navbar = () => {
     return (
-        <Grid  >
-            <Grid.Row only=" tablet computer" >
+        <Grid>
+            <Grid.Row only="tablet computer" style={{ marginBottom: "80px" }}>
                 <Grid.Column computer={14}>
                     <UnderlineLink>
                         <Link to="/">Home</Link>
--- src/application/pages/LandingPage/LandingPage.tsx
+++ src/application/pages/LandingPage/LandingPage.tsx
@@ -1,34 +1,29 @@
 import React from "react";
-import { Navbar } from "./navbar/Navbar";
+import { Navbar } from "../../components/Navbar/Navbar";
 import { Grid, Divider } from "semantic-ui-react";
 import { GetStartedButton, BGImage, CustomDescription, Jumbotron, MarginWrapper } from "./LandingPageCustomStyledTags"
 
 export const LandingPage = () => {
     return (
-        <div>
-
-            <MarginWrapper>
-                <Navbar />
-            </MarginWrapper>
-            <MarginWrapper>
-                <Grid>
-                    <Grid.Row>
-                        <Grid.Column mobile={16} tablet={10} computer={8} largeScreen={6}>
-                            <Jumbotron>Welcome to Architecture Office Manager Tool</Jumbotron>
-                            <CustomDescription>
-                                Carry on your project better with first SCRUM based management tool made specialy for architecture offices.
+        <MarginWrapper>
+            <Navbar />
+            <Grid>
+                <Grid.Row>
+                    <Grid.Column mobile={16} tablet={10} computer={8} largeScreen={6}>
+                        <Jumbotron>Welcome to Architecture Office Manager Tool</Jumbotron>
+                        <CustomDescription>
+                            Carry on your project better with first SCRUM based management tool made specialy for architecture offices.
                             </CustomDescription>
-                        </Grid.Column>
-                        <BGImage />
-                    </Grid.Row>
-                    <Divider hidden />
-                    <Grid.Row>
-                        <Grid.Column width={3}>
-                            <GetStartedButton text="Get Started" />
-                        </Grid.Column >
-                    </Grid.Row>
-                </Grid>
-            </MarginWrapper>
-        </div>
+                    </Grid.Column>
+                    <BGImage />
+                </Grid.Row>
+                <Divider hidden />
+                <Grid.Row>
+                    <Grid.Column mobile={16} tablet={3} computer={3}>
+                        <GetStartedButton text="Get Started" />
+                    </Grid.Column >
+                </Grid.Row>
+            </Grid>
+        </MarginWrapper>
     )
 }

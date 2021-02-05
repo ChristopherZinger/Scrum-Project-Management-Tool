f9df795d75f3cdf164971962c99b9b1fc5c38213
ChristopherZinger <krzysztof.zinger@gmail.com>
2021-02-05 10:44:11 +0100

    add credentials to apollo client

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
   uri: 'http://localhost:8080/graphql',
+  credentials: "include",
   cache: new InMemoryCache()
 });
 

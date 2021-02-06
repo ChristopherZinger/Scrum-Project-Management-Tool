6e9914a91221f82eab194c8aada74c33347cb6e8
ChristopherZinger <krzysztof.zinger@gmail.com>
2021-02-06 22:37:18 +0100

    create spinner and add spinner to hoverstylebutton

---
+++ src/application/atoms/Spinner/Spinner.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import styled from "styled-components"
+
+const SpinnerStyle = styled.div`
+  span {
+    width: 4px;
+    height: 25px;
+    background: white;
+    display: inline-block;
+    position: absolute;
+    -webkit-animation: load1 2s infinite ease-in-out;
+    animation: load1 2s infinite ease-in-out;
+  }
+
+  span:nth-child(1){
+    left: 0px;
+    -webkit-animation-delay: -0.60s;
+  animation-delay: -0.60s;
+  }
+
+  span:nth-child(2){
+    left: 8px;
+    -webkit-animation-delay: -0.40s;
+  animation-delay: -0.40s;
+  }
+
+  span:nth-child(3){
+    left: 16px;
+    -webkit-animation-delay: -0.2s;
+  animation-delay: -0.2s;
+  }
+
+  span:nth-child(4){
+    left: 24px;
+  }
+
+@-webkit-keyframes load1 {
+  30%, 70% {
+    transform: translate(0px)
+  }
+  0%, 20%, 80%, 100% {
+    transform: translate(10px)
+  }
+}
+
+@keyframes load1 {
+  30%, 70% {
+    transform: translate(0px)
+  }
+  0%, 20%, 80%, 100% {
+    transform: translate(8px)
+  }
+}
+`
+
+export const Spinner = () => (
+  <SpinnerStyle style={{
+    position: "relative", width: "50px", height: "20px", left: "50%", transform: "translate(-50%)"
+  }}  >
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+  </SpinnerStyle>
+)

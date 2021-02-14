f66fa685ce6b3f8e7d4f412611ba429de8c3c5ff
ChristopherZin <krzysztof.zinger@gmail.com>
2021-02-14 17:12:14 +0100

    Update setup-redis-and-express-session.ts

--- server/src/core/setup-redis-and-express-session.ts
+++ server/src/core/setup-redis-and-express-session.ts
@@ -6,19 +6,18 @@ import Redis from "ioredis";
 export const redis = new Redis();
 
 export function setupRedisAndExpressSession(app: Express) {
-	const RedisStore = connectRedis(session);
+	//const RedisStore = connectRedis(session);
 	app.use(
 		session({
-			store: new RedisStore({
-				client: redis as any
-			}),
+			//store: new RedisStore({
+			//	client: redis as any
+			//}),
 			name: "qid",
 			secret: process.env.SESSION_SECRET,
 			resave: false,
 			saveUninitialized: false,
 			proxy: true,
 			cookie: {
-				domain: "scrum-arch-service.com",
 				httpOnly: true,
 				secure: process.env.NODE_ENV === "production",
 				maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year

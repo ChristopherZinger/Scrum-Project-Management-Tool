ac1c45ae2b11752f365dfe2830070a1006032b00
ChristopherZinger <krzysztof.zinger@gmail.com>
2021-02-14 14:12:33 +0100

    add env variables

--- server/src/core/create-db-connection.ts
+++ server/src/core/create-db-connection.ts
@@ -1,9 +1,35 @@
 import { createConnection, Connection } from "typeorm";
+import path from "path";
 
 export async function createDBConnection() {
     let conn: null | Connection = null;
     try{
-        conn = await createConnection();
+        conn = await createConnection({
+            type: "postgres",
+            host: process.env.DB_HOST,
+            port: parseInt(process.env.DB_PORT || "5432", 10),
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            synchronize: true,
+            logging: false,
+            entities: [
+                path.join(__dirname,  "../models/**/*.model.{ts,js}")
+            ],
+            migrations: [
+                path.join(__dirname,  "../db/migrations/**/*.{ts,js}")
+                
+            ],
+            subscribers: [
+                path.join(__dirname,  "../subscriber/**/*.{ts,js}")
+                
+            ],
+            cli: {
+                entitiesDir:  path.join(__dirname,  "../models/**/*.model.{ts,js}"),
+                migrationsDir: path.join(__dirname,  "../db/migrations/**/*.{ts,js}"),
+                subscribersDir: path.join(__dirname,  "../subscriber/**/*.{ts,js}")
+            } 
+            });
         console.log("Succes! Connection with db.")
     } catch (err) {
         throw new Error (err)

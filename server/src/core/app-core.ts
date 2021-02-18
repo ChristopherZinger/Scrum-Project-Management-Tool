d5079e5b2cee3c9687aec273ff222aca8f244357
ChristopherZinger <krzysztof.zinger@gmail.com>
2021-02-18 17:51:53 +0100

    fix inversify TYPES  and use injection in user-profile-register.service

--- server/src/core/app-core.ts
+++ server/src/core/app-core.ts
@@ -2,6 +2,7 @@ import { Sequelize } from "sequelize-typescript";
 import { createDBConnection } from "./create-db-connection";
 import { container } from "./create-inversify-container";
 import { Container } from "inversify";
+import { TYPES } from "./TYPES";
 
 export class AppCore {
 	public constructor(
@@ -14,7 +15,7 @@ export const bootAppCore = async function () {
 	const connectionDB = await createDBConnection();
 
 	// bind sequelize
-	container.bind<Sequelize>(Symbol("Sequelize")).toConstantValue(connectionDB);
+	container.bind<Sequelize>(TYPES.Sequelize).toConstantValue(connectionDB);
 
 	return new AppCore(container, connectionDB);
 };

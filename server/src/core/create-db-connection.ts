b1cb8d3c79a25b2a1abed75e0c228a012f00c8f9
ChristopherZinger <krzysztof.zinger@gmail.com>
2021-02-18 10:41:50 +0100

    install adn setup sequelize

--- server/src/core/create-db-connection.ts
+++ server/src/core/create-db-connection.ts
@@ -1,30 +1,20 @@
-import { createConnection, Connection } from "typeorm";
-import path from "path";
+import { UserProfile } from "../models/userProfile/model/UserProfile.model";
+import { Sequelize, SequelizeOptions } from "sequelize-typescript";
+import { User } from "../models/user/model/User.model";
 
 export async function createDBConnection() {
-	let conn: null | Connection = null;
-	try {
-		conn = await createConnection({
-			type: "postgres",
-			host: process.env.DB_HOST,
-			port: parseInt(process.env.DB_PORT || "5432", 10),
-			username: process.env.DB_USERNAME,
-			password: process.env.DB_PASSWORD,
-			database: process.env.DB_NAME,
-			synchronize: false,
-			logging: false,
-			entities: [path.join(__dirname, "../models/**/*.model.{ts,js}")],
-			migrations: [path.join(__dirname, "../db/migrations/**/*.{ts,js}")],
-			subscribers: [path.join(__dirname, "../subscriber/**/*.{ts,js}")],
-			cli: {
-				entitiesDir: path.join(__dirname, "../models/**/*.model.{ts,js}"),
-				migrationsDir: path.join(__dirname, "../db/migrations/**/*.{ts,js}"),
-				subscribersDir: path.join(__dirname, "../subscriber/**/*.{ts,js}")
-			}
-		});
-		console.log("Succes! Connection with db.");
-	} catch (err) {
-		throw new Error(err);
-	}
-	return conn;
+	const options: SequelizeOptions = {
+		database: process.env.DB_NAME,
+		dialect: "postgres",
+		host: process.env.DB_HOST,
+		username: process.env.DB_USERNAME,
+		password: process.env.DB_PASSWORD,
+		benchmark: true
+	};
+
+	const connection = new Sequelize(options);
+
+	connection.addModels([User, UserProfile]);
+
+	return connection;
 }

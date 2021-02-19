22621169a54f70ad553b62e92d33cb99f40bef30
ChristopherZinger <krzysztof.zinger@gmail.com>
2021-02-19 14:26:29 +0100

    refactor request-password-change-email.mutation

--- server/src/models/user/graphql/request-password-change-email.mutation.ts
+++ server/src/models/user/graphql/request-password-change-email.mutation.ts
@@ -1,26 +1,34 @@
-import { Resolver /* Mutation, Arg */ } from "type-graphql";
-// import { sendEmail } from "../../../core/auto-email/send-email";
-// import { createPasswordChangeUrl } from "../../../core/auto-email/create-token-url";
-// import { createConfirmationEmail } from "../../../core/auto-email/emails/create-confirmation-email";
-// import { User } from "../model/User.model";
+import { ApolloError } from "apollo-server-express";
+import { UserRepository } from "./../model/User.repository";
+import { injectable } from "inversify";
+import { Resolver, Mutation, Arg } from "type-graphql";
+import { sendEmail } from "../../../core/auto-email/send-email";
+import { createPasswordChangeUrl } from "../../../core/auto-email/create-token-url";
+import { createConfirmationEmail } from "../../../core/auto-email/emails/create-confirmation-email";
 
+@injectable()
 @Resolver()
 export class RequestPasswordChangeEmailMutation {
-	// @Mutation(() => Boolean)
-	// public async requestPasswordChangeEmail(
-	// 	@Arg("email") email: string
-	// ): Promise<boolean> {
-	// 	const user = await User.findOne({ where: { email } });
-	// 	if (!user) {
-	// 		console.warn("Could not find user with email: ", email);
-	// 		return false;
-	// 	}
-	// 	const passwordChangeUrl = await createPasswordChangeUrl(user.id);
-	// 	const confirmationEmail = createConfirmationEmail(
-	// 		user.email,
-	// 		passwordChangeUrl
-	// 	);
-	// 	sendEmail(confirmationEmail);
-	// 	return true;
-	// }
+	constructor(private userRepository: UserRepository) {}
+	@Mutation(() => Boolean)
+	public async requestPasswordChangeEmail(
+		@Arg("email") email: string
+	): Promise<boolean> {
+		const user = await this.userRepository.findByEmail(email);
+
+		if (!user) {
+			console.error(`Can't find user with email: '${email}'`);
+			throw new ApolloError(
+				"Can't find user with this email.",
+				"USER_DOES_NOT_EXIST"
+			);
+		}
+		const passwordChangeUrl = await createPasswordChangeUrl(user.id);
+		const confirmationEmail = createConfirmationEmail(
+			user.email,
+			passwordChangeUrl
+		);
+		sendEmail(confirmationEmail);
+		return true;
+	}
 }

b83433290c8c77ce99aa944cece4df7e6b2a95de
ChristopherZinger <krzysztof.zinger@gmail.com>
2021-02-19 14:43:12 +0100

    refactor request-confirmation-email.mutation

--- server/src/models/user/graphql/request-confirmation-email.mutation.ts
+++ server/src/models/user/graphql/request-confirmation-email.mutation.ts
@@ -1,37 +1,47 @@
-// import { ContextType } from "../../../core/context/context-type";
-import { Resolver /* Ctx, Mutation, Authorized */ } from "type-graphql";
-// import { sendEmail } from "../../../core/auto-email/send-email";
-// import { createConfirmationUrl } from "../../../core/auto-email/create-token-url";
-// import { createConfirmationEmail } from "../../../core/auto-email/emails/create-confirmation-email";
-// import { User } from "../model/User.model";
+import { UserRepository } from "./../model/User.repository";
+import { injectable } from "inversify";
+import { ApolloError } from "apollo-server-express";
+import { ContextType } from "../../../core/context/context-type";
+import { Resolver, Ctx, Mutation, Authorized } from "type-graphql";
+import { sendEmail } from "../../../core/auto-email/send-email";
+import { createConfirmationUrl } from "../../../core/auto-email/create-token-url";
+import { createConfirmationEmail } from "../../../core/auto-email/emails/create-confirmation-email";
 
+@injectable()
 @Resolver()
 export class RequestConfirmationEmailMutation {
-	// @Authorized()
-	// @Mutation(() => Boolean)
-	// public async requestConfirmationEmail(
-	// 	@Ctx() context: ContextType
-	// ): Promise<boolean> {
-	// 	const email = context.session.user?.email;
-	// 	if (!email) {
-	// 		console.warn("Could not find email or user for this session");
-	// 		return false;
-	// 	}
-	// 	const user = await User.findOne({ where: { email } });
-	// 	if (!user) {
-	// 		console.warn("Could not find user with email: ", email);
-	// 		return false;
-	// 	}
-	// 	if (user.emailConfirmed) {
-	// 		console.warn(`User with email ${user.email} already already confirmed.`);
-	// 		return false;
-	// 	}
-	// 	const confirmationUrl = await createConfirmationUrl(user.id);
-	// 	const confirmationEmail = createConfirmationEmail(
-	// 		user.email,
-	// 		confirmationUrl
-	// 	);
-	// 	sendEmail(confirmationEmail);
-	// 	return true;
-	// }
+	public constructor(private userRepository: UserRepository) {}
+	@Authorized()
+	@Mutation(() => Boolean)
+	public async requestConfirmationEmail(
+		@Ctx() context: ContextType
+	): Promise<boolean> {
+		const email = context.session.user?.email;
+		if (!email) {
+			console.error("Could not find email or user for this session");
+			throw new ApolloError(
+				"Could not find user email in this session.",
+				"EMAIL_IS_MISSING"
+			);
+		}
+		const user = await this.userRepository.findByEmail(email);
+		if (!user) {
+			console.error("Could not find user with email: ", email);
+			return false;
+		}
+		if (user.emailConfirmed !== null) {
+			console.error(`User with email ${user.email} already already confirmed.`);
+			throw new ApolloError(
+				"User alreay confirmed the email.",
+				"EMAIL_IS_CONFIRMED"
+			);
+		}
+		const confirmationUrl = await createConfirmationUrl(user.id);
+		const confirmationEmail = createConfirmationEmail(
+			user.email,
+			confirmationUrl
+		);
+		sendEmail(confirmationEmail);
+		return true;
+	}
 }

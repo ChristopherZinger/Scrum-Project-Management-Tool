82f7375cc80faba9d1f273b8767a62e91665011e
ChristopherZinger <krzysztof.zinger@gmail.com>
2021-02-22 16:41:44 +0100

    remove container

--- src/application/pages/Dashboard/Admin/Admin.tsx
+++ src/application/pages/Dashboard/Admin/Admin.tsx
@@ -6,27 +6,25 @@ import { Heading } from "../../../atoms/style";
 
 export const Admin = () => {
   return (
-    <Container>
-      <Grid stackable>
-        <Grid.Row>
-          <Heading.H2>Admin</Heading.H2>
-        </Grid.Row>
+    <Grid stackable>
+      <Grid.Row>
+        <Heading.H2>Admin</Heading.H2>
+      </Grid.Row>
 
-        <Grid.Row columns="equal">
-          <Grid.Column>
-            <DashboardCard title="Projects">
-              <p>List of your projects</p>
-            </DashboardCard>
-          </Grid.Column>
+      <Grid.Row columns="equal">
+        <Grid.Column>
+          <DashboardCard title="Projects">
+            <p>List of your projects</p>
+          </DashboardCard>
+        </Grid.Column>
 
-          <Grid.Column>
-            <DashboardCard title="Team">
-              <p>List of your team members</p>
-            </DashboardCard>
-          </Grid.Column>
+        <Grid.Column>
+          <DashboardCard title="Team">
+            <p>List of your team members</p>
+          </DashboardCard>
+        </Grid.Column>
 
-        </Grid.Row>
-      </Grid>
-    </Container>
+      </Grid.Row>
+    </Grid>
   )
 }

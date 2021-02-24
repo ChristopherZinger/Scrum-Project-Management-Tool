0d5deafa5512c4e548e166e1763216cb874c9d9b
ChristopherZinger <krzysztof.zinger@gmail.com>
2021-02-24 09:27:17 +0100

    make logout mutation injectable

--- server/src/models/userProfile/graphql/logout.mutation.ts
+++ server/src/models/userProfile/graphql/logout.mutation.ts
@@ -1,7 +1,9 @@
 import { Resolver, Mutation, Ctx } from "type-graphql";
 import { ContextType } from "../../../core/context/context-type";
 import { CONST } from "../../../core/CONST";
+import { injectable } from "inversify";
 
+@injectable()
 @Resolver()
 export class LogoutMutation {
 	@Mutation(() => Boolean)
@@ -12,7 +14,11 @@ export class LogoutMutation {
 					console.error(err);
 					rej(false);
 				}
-				context.response.clearCookie(CONST.sessionCookieName);
+
+				context.response.clearCookie(
+					CONST.sessionCookieName,
+					CONST.sessionCookieOptions
+				);
 				res(true);
 			})
 		);

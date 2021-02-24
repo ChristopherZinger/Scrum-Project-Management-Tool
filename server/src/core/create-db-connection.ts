df6b24795e58211a6dbf7eb51b1114c73b866b9d
ChristopherZinger <krzysztof.zinger@gmail.com>
2021-02-24 09:18:12 +0100

    remove loggin from squelize

--- server/src/core/create-db-connection.ts
+++ server/src/core/create-db-connection.ts
@@ -7,6 +7,7 @@ export async function createDBConnection() {
 	const options: SequelizeOptions = {
 		database: process.env.DB_NAME,
 		dialect: "postgres",
+		logging: false,
 		host: process.env.DB_HOST,
 		username: process.env.DB_USERNAME,
 		password: process.env.DB_PASSWORD,

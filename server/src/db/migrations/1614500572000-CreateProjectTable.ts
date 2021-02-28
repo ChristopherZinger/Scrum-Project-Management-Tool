07c2a860a685ff2d6b474cad3c577db99796db4c
ChristopherZinger <krzysztof.zinger@gmail.com>
2021-02-28 09:42:16 +0100

    add project migration

---
+++ server/src/db/migrations/1614500572000-CreateProjectTable.ts
@@ -0,0 +1,49 @@
+import { QueryInterface, DataTypes } from "sequelize";
+
+const migration = {
+	up: (queryInterface: QueryInterface) =>
+		queryInterface.sequelize.transaction(async transaction => {
+			await queryInterface.createTable(
+				"Project",
+				{
+					id: {
+						type: DataTypes.INTEGER,
+						primaryKey: true,
+						autoIncrement: true
+					},
+					title: {
+						type: DataTypes.STRING,
+						allowNull: false
+					},
+					pid: {
+						// public id
+						type: DataTypes.STRING,
+						allowNull: true
+					},
+					companyId: {
+						type: DataTypes.INTEGER,
+						allowNull: false,
+						references: {
+							model: "Company",
+							key: "id"
+						}
+					},
+					createdAt: {
+						type: DataTypes.DATE,
+						allowNull: false
+					},
+					updatedAt: {
+						type: DataTypes.DATE,
+						allowNull: false
+					}
+				},
+				{ transaction }
+			);
+		}),
+
+	down: (queryInterface: QueryInterface) => {
+		return queryInterface.dropTable("Project");
+	}
+};
+
+export default migration;

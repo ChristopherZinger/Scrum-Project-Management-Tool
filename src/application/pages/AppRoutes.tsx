5ef75d74dcde7899312856e36a63dc4bb4d7694f
ChristopherZinger <krzysztof.zinger@gmail.com>
2021-03-03 11:12:28 +0100

    split public and auth routes in separate components and remove redundant props

--- src/application/pages/AppRoutes.tsx
+++ src/application/pages/AppRoutes.tsx
@@ -23,48 +23,37 @@ export enum RoutesMain {
 export function AppRoutes () {
     const { user } = useContext(UserAuthStateContext);
 
-    const routes = [];
-
-    if (user) {
-        routes.push(
-            <Route path={[RoutesMain.HOME, RoutesMain.DASHBOARD]}>
-                <Dashboard user={user} />
-            </Route>)
-    } else {
-        routes.push(
-            ...[
-                (
-                    <Route path={RoutesMain.LOGIN} >
-                        <Login />
-                    </Route>
-                ),
-                (
-                    <Route path={RoutesMain.REGISTER_PROFILE} >
-                        <RegisterUser />
-                    </Route>
-                ),
-                (
-                    <Route path={RoutesMain.REGISTER_COMPANY} >
-                        <RegisterCompany />
-                    </Route>
-                ),
-                (
-                    <Route path="/register-with-invitation/:companyId/:token" component={RegisterWithInvitation} />
-                ),
-                (
-                    <Route path={RoutesMain.HOME}>
-                        <LandingPage />
-                    </Route>
-                )
-            ]
-        )
-    }
-
     return (
         <Router>
             <Switch>
-                {routes.map(route => route)}
+                {user && <AuthRoutes />}
+                {!user && <PublicRoutes />}
             </Switch>
         </Router>
     )
 }
+
+const AuthRoutes = () =>
+    <Route path={[RoutesMain.HOME, RoutesMain.DASHBOARD]}>
+        <Dashboard />
+    </Route>
+
+const PublicRoutes = () =>
+    <>
+        <Route path={RoutesMain.LOGIN} >
+            <Login />
+        </Route>
+        <Route path={RoutesMain.REGISTER_PROFILE} >
+            <RegisterUser />
+        </Route>
+
+        <Route path={RoutesMain.REGISTER_COMPANY} >
+            <RegisterCompany />
+        </Route>
+
+        <Route path="/register-with-invitation/:companyId/:token" component={RegisterWithInvitation} />
+
+        <Route path={RoutesMain.HOME}>
+            <LandingPage />
+        </Route>
+    </>

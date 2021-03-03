10103f72a94ac00e072bcda0616724095cad35df
ChristopherZinger <krzysztof.zinger@gmail.com>
2021-03-03 10:59:32 +0100

    fix rect router conditional reder error

--- src/application/pages/AppRoutes.tsx
+++ src/application/pages/AppRoutes.tsx
@@ -23,32 +23,47 @@ export enum RoutesMain {
 export function AppRoutes () {
     const { user } = useContext(UserAuthStateContext);
 
+    const routes = [];
+
+    if (user) {
+        routes.push(
+            <Route path={[RoutesMain.HOME, RoutesMain.DASHBOARD]}>
+                <Dashboard user={user} />
+            </Route>)
+    } else {
+        routes.push(
+            ...[
+                (
+                    <Route path={RoutesMain.LOGIN} >
+                        <Login />
+                    </Route>
+                ),
+                (
+                    <Route path={RoutesMain.REGISTER_PROFILE} >
+                        <RegisterUser />
+                    </Route>
+                ),
+                (
+                    <Route path={RoutesMain.REGISTER_COMPANY} >
+                        <RegisterCompany />
+                    </Route>
+                ),
+                (
+                    <Route path="/register-with-invitation/:companyId/:token" component={RegisterWithInvitation} />
+                ),
+                (
+                    <Route path={RoutesMain.HOME}>
+                        <LandingPage />
+                    </Route>
+                )
+            ]
+        )
+    }
+
     return (
         <Router>
             <Switch>
-                {user && (
-                    <Route path={[RoutesMain.HOME, RoutesMain.DASHBOARD]}>
-                        <Dashboard user={user} />
-                    </Route>
-                )}
-
-                {!user && (
-                    <>
-                        <Route path={RoutesMain.LOGIN} >
-                            <Login />
-                        </Route>
-                        <Route path={RoutesMain.REGISTER_PROFILE} >
-                            <RegisterUser />
-                        </Route>
-                        <Route path={RoutesMain.REGISTER_COMPANY} >
-                            <RegisterCompany />
-                        </Route>
-                        <Route path="/register-with-invitation/:companyId/:token" component={RegisterWithInvitation} />
-                        <Route path={RoutesMain.HOME}>
-                            <LandingPage />
-                        </Route>
-                    </>
-                )}
+                {routes.map(route => route)}
             </Switch>
         </Router>
     )

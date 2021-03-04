f7e0768d8160e1c95eba7f3af8d3f4deffd0121c
ChristopherZinger <krzysztof.zinger@gmail.com>
2021-03-04 09:02:49 +0100

    add update story form

--- src/application/components/BacklogCard/StoryList.tsx
+++ src/application/components/BacklogCard/StoryList.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import { ProjectQuery, StoryResponseType } from "../../../types.d";
+import { ProjectQuery, StoryResponseType, useUpdateStoryMutation, StoryStatus } from "../../../types.d";
 import { Grid } from "semantic-ui-react";
 import { Colors } from "../../../global-styles/global-styles";
 import styled from "styled-components";
 import { Modal } from "../../atoms/Modal/Modal";
+import { Formik, Form, Field } from "formik";
 
 const LabelColor = styled.div`
   transform: translateY(4px);
@@ -27,6 +28,7 @@ export const StoryList = (props: { stories: ProjectQuery["project"]["backlog"] }
 
 const StoryListItem = (props: { story: StoryResponseType }) => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [updateStory, updateStoryResult] = useUpdateStoryMutation();
 
   return (
     <>
@@ -51,10 +53,69 @@ const StoryListItem = (props: { story: StoryResponseType }) => {
             {props.story.title}
           </Modal.Header>
           <Modal.Content>
+            <Formik
+              initialValues={{ status: props.story.status || "", addToActiveSprint: false }}
+              onSubmit={async (values) => {
 
+                // make sure status exists on StoryStatus
+                let status = props.story.status;
+                if (Object.values(StoryStatus).includes(values.status as StoryStatus)) {
+                  status = values.status as StoryStatus
+                }
+
+                try {
+                  const updatedStory = await updateStory({
+                    variables: {
+                      data: {
+                        storyId: props.story.id,
+                        status: status,
+                        addToActiveSprint: values.addToActiveSprint
+                      }
+                    }
+                  })
+                  console.log(updatedStory)
+                } catch (err) {
+                  console.log(err.networkError)
+                  console.log(err.graphQLErrors)
+                }
+                setModalIsOpen(false)
+              }}
+            >
+
+              {() =>
+                <Form id="update-story-form">
+                  <label>Add to Active Sprint</label>
+                  <Field type="checkbox" name="addToActiveSprint" />
+
+                  <Field as="select" name="status">
+                    {Object.keys(StoryStatus).map((key, i) =>
+                      <option
+                        key={i}
+                        value={StoryStatus[key as keyof typeof StoryStatus]} >
+                        {key}
+                      </option>
+                    )}
+                    <option value="" >none </option>
+                  </Field>
+                </Form>
+              }
+            </Formik>
           </Modal.Content>
           <Modal.Actions>
-            <button onClick={() => setModalIsOpen(false)}>Cancel</button>
+            <button
+              onClick={() => setModalIsOpen(false)}
+              disabled={updateStoryResult.loading}
+            >
+              Cancel
+              </button>
+
+            <button
+              type="submit"
+              form="update-story-form"
+              disabled={updateStoryResult.loading}
+            >
+              Update
+            </button>
           </Modal.Actions>
         </Modal>
       )}

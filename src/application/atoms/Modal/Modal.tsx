8199513d06df198aaec4f07e3447cc490073723d
ChristopherZinger <krzysztof.zinger@gmail.com>
2021-03-09 10:50:54 +0100

    add modal mobile styles

--- src/application/atoms/Modal/Modal.tsx
+++ src/application/atoms/Modal/Modal.tsx
@@ -3,15 +3,45 @@ import { Modal as SemanticUIModal } from "semantic-ui-react"
 import { Colors } from "../../../global-styles/global-styles";
 
 export const Modal = styled(SemanticUIModal)`
+
   &&& {
-    border-radius: 0px;
+
     border: 2px solid black;
+    border-radius: 0px;
+
+    @media only screen and (max-width: 767px){
+      border: none;
+      height: 100vh;
+      width: 100vw;
+      top: 0px;
+      left: 0px;
+      margin-top: 0px !important;
+      margin: 0px !important;
+      border: 5px solid coral;
+
+      display: flex !important;
+      flex-direction: column !important;
+      justify-content: space-between !important;
+
+      .actions {
+        display: flex !important;
+        align-items: stretch !important;
+        
+      }
+      button {
+        flex-grow: 1;
+        flex-basis: 0;
+      }
+      button:first-child{
+        border-left: none !important;
+      }
+    }
 
     .actions {
       background: white;
-      border-top:1px solid black;
+      border-top:1px solid #7a2a2a;
       height: 40px;
-      padding: 0px;
+      padding: 0px !important;
     }
 
     .header {

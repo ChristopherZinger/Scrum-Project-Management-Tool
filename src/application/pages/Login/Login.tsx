960be3cab8283044da949147237d744be2050635
ChristopherZinger <krzysztof.zinger@gmail.com>
2021-03-10 12:04:13 +0100

    simplify error handling

--- src/application/pages/Login/Login.tsx
+++ src/application/pages/Login/Login.tsx
@@ -27,99 +27,86 @@ const AuthFormWrapper = styled(CenteredDiv)`
 `
 
 export const Login = () => {
-    const history = useHistory();
-    const [login, { loading, error }] = useLoginMutation();
-    const dispatch = useContext(UserAuthDispatchContext);
+  const history = useHistory();
+  const [login, { loading, error }] = useLoginMutation();
+  const dispatch = useContext(UserAuthDispatchContext);
 
-    const LoginValidationSchema = Yup.object().shape({
-        password: Yup.string()
-            .required('Required'),
-        email: Yup.string().email('Invalid email').required('Required'),
-    });
+  console.log(error?.graphQLErrors)
+  console.log(error?.message)
 
-    return (
-        <MarginWrapper>
-            <Navbar />
-            <Grid>
-                <Grid.Row>
-                    <Grid.Column width={16}>
-                        <Heading.H1 style={{
-                            textAlign: "center",
-                            fontFamily: decorativeFont,
-                            marginBottom: "50px"
-                        }}>Let's SCRUM!</Heading.H1>
-                    </Grid.Column>
-                </Grid.Row>
-                <Grid.Row>
-                    <Grid.Column only="computer" computer={8}  >
-                        <Image style={{ left: "50%", transform: "translate(-50%)" }} size="large" src={accessAccountImg} />
-                    </Grid.Column>
+  const LoginValidationSchema = Yup.object().shape({
+    password: Yup.string()
+      .required('Required'),
+    email: Yup.string().email('Invalid email').required('Required'),
+  });
 
-                    <Grid.Column mobile={16} computer={8} >
+  return (
+    <MarginWrapper>
+      <Navbar />
+      <Grid>
+        <Grid.Row>
+          <Grid.Column width={16}>
+            <Heading.H1 style={{
+              textAlign: "center",
+              fontFamily: decorativeFont,
+              marginBottom: "50px"
+            }}>Let's SCRUM!</Heading.H1>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column only="computer" computer={8}  >
+            <Image style={{ left: "50%", transform: "translate(-50%)" }} size="large" src={accessAccountImg} />
+          </Grid.Column>
 
-                        <AuthFormWrapper>
-                            <Formik
-                                initialValues={{ email: "", password: "" }}
-                                validationSchema={LoginValidationSchema}
-                                onSubmit={async (values) => {
-                                    try {
-                                        const { data } = await login({
-                                            variables: {
-                                                data: {
-                                                    email: values.email,
-                                                    password: values.password
-                                                }
-                                            }
-                                        })
-                                        if (data?.login) {
-                                            dispatch({ type: "login", user: data.login })
-                                            history.push("/")
-                                            toast.success("Hello! You are logged in.")
-                                        }
-                                    } catch (err) {
-                                        if (error?.graphQLErrors) {
-                                            for (const gqlError of error.graphQLErrors) {
-                                                if (!gqlError.extensions) continue;
-                                                switch (gqlError.extensions.code) {
-                                                    case "WRONG_CREDENTIALS":
-                                                        toast.error("Incorrect email or password.")
-                                                        break;
-                                                    default:
-                                                        break;
-                                                }
-                                            }
-                                        } else {
-                                            toast.error("Sorry, Something went wrong.")
-                                        }
-                                    }
-                                }}>
-                                {({ errors, touched }) => (
-                                    <Form>
-                                        <div style={{ marginBottom: "50px" }}>
-                                            <label>Email</label>
-                                            <Input name="email" placeholder="eg: kate@email.com" type="text" />
-                                            <InputError name="email" />
-                                        </div>
+          <Grid.Column mobile={16} computer={8} >
 
-                                        <div style={{ marginBottom: "50px" }}>
-                                            <label>Password</label>
-                                            <Input name="password" type="password" />
-                                            <InputError name="password" />
-                                        </div>
+            <AuthFormWrapper>
+              <Formik
+                initialValues={{ email: "", password: "" }}
+                validationSchema={LoginValidationSchema}
+                onSubmit={async (values) => {
+                  const { data } = await login({
+                    variables: {
+                      data: {
+                        email: values.email,
+                        password: values.password
+                      }
+                    }
+                  })
+                  if (data?.login) {
+                    dispatch({ type: "login", user: data.login })
+                    history.push("/")
+                    toast.success("Hello! You are logged in.")
+                  }
+                }}>
+                {({ errors, touched }) => (
+                  <Form>
+                    <div style={{ marginBottom: "50px" }}>
+                      <label>Email</label>
+                      <Input name="email" placeholder="eg: kate@email.com" type="text" />
+                      <InputError name="email" />
+                    </div>
 
-                                        <HoverStyleButton
-                                            type="submit"
-                                            text="Loading"
-                                            isLoading={loading}
-                                            disabled={loading}
-                                        />
-                                    </Form>
-                                )}
-                            </Formik>
-                        </AuthFormWrapper>
-                    </Grid.Column>
-                </Grid.Row>
-            </Grid>
-        </MarginWrapper >
-    )
+                    <div style={{ marginBottom: "50px" }}>
+                      <label>Password</label>
+                      <Input name="password" type="password" />
+                      <InputError name="password" />
+                    </div>
+
+                    <HoverStyleButton
+                      type="submit"
+                      text="Loading"
+                      isLoading={loading}
+                      disabled={loading}
+                    />
+                  </Form>
+                )}
+              </Formik>
+            </AuthFormWrapper>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+      {error && toast.error(error.message || "Upss! Something went wrong.")}
+    </MarginWrapper >
+  )
 }
